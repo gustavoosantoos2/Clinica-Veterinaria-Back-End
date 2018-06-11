8a4fa3ed623e63c6e8362fc9457a9db38f1eb6e4
DiegoSeronato <DiegoSeronato@192.168.25.5>
2018-06-11 00:10:31 -0300

    Crud Animal

--- ClinicaVeterinaria/src/services/AnimaisServlet.java
+++ ClinicaVeterinaria/src/services/AnimaisServlet.java
@@ -20,11 +20,11 @@ import model.utils.Serializer;
 @WebServlet("/AnimaisServlet")
 public class AnimaisServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	//doGet será para listar e buscar por ID...
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String action = request.getParameter("action");
-		
+
 		if (action.equalsIgnoreCase("listar")) {
 			try {
 				listar(request, response);
@@ -42,29 +42,72 @@ public class AnimaisServlet extends HttpServlet {
 	//doPost será para inclusões, updates e deletes...
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String action = request.getParameter("action");
-		if (action.equalsIgnoreCase("inclusao")) {
-			Serializer serializer = new Serializer();
-			Animal animal = serializer.desserialize(request.getReader(), Animal.class);
-			AnimalService service = new AnimalService(new AnimalDAO());
-			try {
-				service.salvar(animal);
-				response.setStatus(HttpServletResponse.SC_OK);
-			} catch (Exception e) {
-				e.printStackTrace();
-				response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		Long id;
+		try {
+			id = Long.parseLong(request.getParameter("id"));
+		}catch (Exception e) {
+			id = new Long(-1);
+		}
+		if(id >= 0) {
+			if (action.equalsIgnoreCase("incluir")) {
+
+				Serializer serializer = new Serializer();
+				Animal animal = serializer.desserialize(request.getReader(), Animal.class);
+				AnimalService service = new AnimalService(new AnimalDAO());
+				try {
+					service.salvar(animal);
+					response.setStatus(HttpServletResponse.SC_OK);
+				} catch (Exception e) {
+					e.printStackTrace();
+					response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				}
+
+			} else if (action.equalsIgnoreCase("atualizar")) {
+				Serializer serializer = new Serializer();
+				Animal animal = serializer.desserialize(request.getReader(), Animal.class);
+				AnimalService service = new AnimalService(new AnimalDAO());
+				try {
+					service.atualizar(animal);
+					response.setStatus(HttpServletResponse.SC_OK);
+				} catch (Exception e) {
+					e.printStackTrace();
+					response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				}
 			}
+			else if (action.equalsIgnoreCase("deletar")) {
+				AnimalService service = new AnimalService(new AnimalDAO());
+				Animal a = null;
+				try {
+					a = service.buscar(id);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				if ( a !=null && a.getId() >= 0) {
+					try {
+						service.remover(a.getId());
+						response.setStatus(HttpServletResponse.SC_OK);
+					} catch (Exception e) {
+						e.printStackTrace();
+						response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+					}
+				}else {
+					response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+				}
+			}
+		}else {
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
 	private void listar(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+
 		try {
 			AnimalService service = new AnimalService(new AnimalDAO());
 			List<Animal> animais = service.listar();
-			
+
 			Serializer serializer = new Serializer();
 			String json = serializer.serialize(animais);
-			
+
 			response.setStatus(HttpServletResponse.SC_OK);
 			response.getWriter().write(json);
 		} catch (Exception e) {
@@ -72,15 +115,15 @@ public class AnimaisServlet extends HttpServlet {
 			throw e;
 		}
 	}
-	
+
 	private void buscar(HttpServletRequest request, HttpServletResponse response, Long id) {
 		try {
 			AnimalService service = new AnimalService(new AnimalDAO());
 			Animal animal = service.buscar(id);
-			
+
 			Serializer serializer = new Serializer();
 			String json = serializer.serialize(animal);
-			
+
 			response.setStatus(HttpServletResponse.SC_OK);
 			response.getWriter().write(json);
 		} catch (Exception e) {
